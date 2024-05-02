71e6dd0b6be4d6e2006700868bb0af3104b18126
saikiranAnnam <saikiranannam23@gmail.com>
2024-05-02 15:30:52 +0530

    intialzied socket listeners

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -10,6 +10,8 @@ async function init() {
   httpServer.listen(PORT, () => {
     console.log(`HTTP Server started at PORT: ${PORT}`);
   });
+  
+  socketService.initListeners();
 }
 
 init();
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -8,9 +8,20 @@ class SocketService {
     this._io = new Server();
   }
 
-  get io(){
-    return this._io;
+  public initListeners() {
+    const io = this.io;
+    console.log("Init socket listeners....");
+    io.on("connect", (socket) => {
+      console.log(`New Socket is Connected`, socket.id);
 
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New Message Recieved", message);
+      });
+    });
+  }
+
+  get io() {
+    return this._io;
   }
 }
 
