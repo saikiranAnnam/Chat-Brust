8fa59b5170ded1e6712a522ce81b81767575e183
saikiranAnnam <saikiranannam23@gmail.com>
2024-05-02 12:04:21 +0530

    project intialization and added boiler plate code

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,17 @@
+import { Server } from "socket.io";
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log("Init Socket Server..");
+    this._io = new Server();
+  }
+
+  get io(){
+    return this._io;
+
+  }
+}
+
+export default SocketService;
